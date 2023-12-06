ddef62bc104b740d3ebf0ddc52cd8a0a8c6890ae
jacob.pickett <jacob@pickett.com>
2023-12-06 13:12:17 -0500

    Reseeding and connecting to cluster

--- seeds/index.js
+++ seeds/index.js
@@ -1,9 +1,13 @@
+if (process.env.NODE_ENV !== "production") {
+	require("dotenv").config();
+}
+
 const mongoose = require("mongoose");
 const cities = require("./cities");
 const { places, descriptors } = require("./seedHelpers");
 const Campground = require("../models/campground");
 
-mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp", {
+mongoose.connect(process.env.DB_URL, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 });
